01817630c049d1ec5739e4d3354bdd301a90abe9
Cosgos <cosmos.disrupt@gmail.com>
2025-05-21 15:57:08 -0400

    Added setup.lua
    Added auto updating with setup.lua

--- cosgOS/setup.lua
+++ cosgOS/setup.lua
@@ -1,12 +1,47 @@
 -- Checks if the current version file exists, and sets version to the version file
 
-local currentVersion = fs.open("currentVersion.txt")
+local localVersion = fs.open("localVersion.txt")
 local version
-if currentVersion then
-    version = currentVersion
-    currentVersion.close()
+if localVersion then
+    version = localVersion
+    localVersion.close()
 else
     version = 0
 end
+-- gets the current version and saves it to the currentVersion file
+shell.run("wget https://github.com/Cosgos/cosgOS/blob/main/cosgOS/version.txt currentVersion.txt")
 
-shell.run("wget ")
+-- sets the currentVersion variable to the version in the currentVersion file
+local file = fs.open("currentVersion.txt", "r")
+local currentVersion = file.readAll()
+file.close()
+
+shell.run("delete currentVersion.txt")
+
+if currentVersion ~= version then
+    print("New version detected. Updating.")
+    --deletes old files
+    sleep(1)
+    print("Deleting deprecated files...")
+    shell.run("delete localVersion.txt")
+    shell.run("delete UI.lua")
+    shell.run("delete applicationMenu.lua")
+    shell.run("delete commandPrompt.lua")
+    shell.run("delete back.lua")
+    shell.run("delete startup.lua")
+    --reinstalls the files from github
+    sleep(1)
+    print("Installing new version...")
+    shell.run("wget https://raw.githubusercontent.com/Cosgos/cosgOS/main/startup.lua startup.lua")
+    shell.run("wget https://raw.githubusercontent.com/Cosgos/cosgOS/main/back.lua back.lua")
+    shell.run("https://raw.githubusercontent.com/Cosgos/cosgOS/main/cosgOS/UI.lua cosgOS/UI.lua")
+    shell.run("https://raw.githubusercontent.com/Cosgos/cosgOS/main/cosgOS/commandPrompt.lua cosgOS/commandPrompt.lua")
+    shell.run("https://raw.githubusercontent.com/Cosgos/cosgOS/main/cosgOS/applicationMenu.lua cosgOS/applicationMenu.lua")
+
+    --Update the version file
+    local newVersion = fs.open("localVersion.txt", "w")
+    newVersion.write(currentVersion)
+    newVersion.close()
+end
+
+os.reboot()
